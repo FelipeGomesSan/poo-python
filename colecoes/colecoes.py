b9dd328417653c3de03cdba9780fe391ea600412
Felipe Gomes <felipe_gomessantos@hotmail.com>
2015-09-30 08:47:23 -0300

    Update colecoes.py

--- colecoes/colecoes.py
+++ colecoes/colecoes.py
@@ -6,7 +6,15 @@ if __name__=='__main__':
     gomes_igual = Homem('Gomes')
     gomes_identico=gomes
     selina=Mulher('Selina')
+    igor=Homem('Igor')
     print(gomes is gomes_igual)
     print(gomes is gomes_identico)
     print(gomes == gomes_igual)
     print(gomes == gomes_identico)
+
+    conjunto=set((gomes,gomes_identico,gomes_igual,selina))
+    conjunto2=set((igor,selina))
+    print(conjunto)
+    print(gomes is conjunto)
+    print(gomes is conjunto2)
+    print(conjunto+conjunto2)
